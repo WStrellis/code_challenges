b67b2e16bda4a7588880cee41b76c1fcbc1833ab
WStrellis <wstrellis@gmail.com>
2020-02-12 09:49:53 -0500

    next will be got1

---
+++ hacker_rank/game_of_thrones_I.js
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/game-of-thrones/problem?h_r=next-challenge&h_v=zen&isFullScreen=false&h_r=next-challenge&h_v=zen
+
+/*
+Complete the gameOfThrones function below to determine whether a given string can be rearranged into a palindrome. If it is possible, return YES, otherwise return NO.
+
+gameOfThrones has the following parameter(s):
+
+s: a string to analyze
+Input Format
+
+A single line which contains s, the input string.
+
+s only contains [a-z]
+
+Output:
+'YES' || 'NO'
+*/
