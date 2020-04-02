d7b24e41c3354965656a84b9498297296da1ad97
WStrellis <wstrellis@gmail.com>
2020-04-02 08:26:36 -0400

    still working on bucket_paint

--- hacker_rank/medium/bucket_paint.js
+++ hacker_rank/medium/bucket_paint.js
@@ -10,16 +10,61 @@ function strokesRequired(picture) {
 
     function findUnvisited(p, visited) {
         //function to find unvisited cells
-        return // return coordinates of first found unvisited cell or null
+        let stringified = null
+        outer: for (let y = 0; y < p.length; y++) {
+            for (let x = 0; x < p[y].length; x++) {
+                if (!visited.has(`[${y},${x}]`)) {
+                    stringified = [y, x]
+                    break outer
+                }
+            }
+        }
+        return stringified // return coordinates of first found unvisited cell or null
     }
 
-    function getNeighbors(current, picture) {
+    function getNeighbors(current, picture, visited) {
         // finds neighbor cells with same color
         const neighborCells = []
+        let y = current[0]
+        let x = current[1]
         //Check n
+        if (y > 0) {
+            if (
+                picture[y - 1][x] === picture[y][x] &&
+                !visited.has(`[${y - 1},${x}]`)
+            ) {
+                neighborCells.push([y - 1, x])
+            }
+        }
         //Check s
+        if (y < picture.length - 1) {
+            if (
+                picture[y + 1][x] === picture[y][x] &&
+                !visited.has(`[${y + 1},${x}]`)
+            ) {
+                neighborCells.push([y + 1, x])
+            }
+        }
+
         //Check e
+        if (x > 0) {
+            if (
+                picture[y][x - 1] === picture[y][x] &&
+                !visited.has(`[${y},${x - 1}]`)
+            ) {
+                neighborCells.push([y, x - 1])
+            }
+        }
         //Check w
+        if (x < picture[0].length - 1) {
+            if (
+                picture[y][x + 1] === picture[y][x] &&
+                !visited.has(`[${y},${x + 1}]`)
+            ) {
+                neighborCells.push([y, x + 1])
+            }
+        }
+
         return neighborCells
     }
 
@@ -41,11 +86,14 @@ function strokesRequired(picture) {
             if (unvisited) {
                 current = unvisited
             } else {
+                // all cells have been painted
                 running = false
             }
         } else {
             // add current cell to visited
+            visited.add(JSON.stringify(current))
             // get neighbors of current cell that are same color and add to stack
+            stack = [...stack, ...getNeighbors(current, picture, visited)]
         }
     }
 
